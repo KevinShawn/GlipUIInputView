6d6e833ab58334f00a1a4b28fa522fb7f2b2a139
Leon Xiao <leon.xiao@ringcentral.com>
2019-01-06 19:51:17 +0800

    inital project

---
+++ Example/Pods/Target Support Files/GlipUIInputView/GlipUIInputView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GlipUIInputView : NSObject
+@end
+@implementation PodsDummy_GlipUIInputView
+@end
---
+++ Example/Pods/Target Support Files/Pods-GlipUIInputView_Example/Pods-GlipUIInputView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GlipUIInputView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GlipUIInputView_Example
+@end
